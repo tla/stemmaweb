5ba6c2b4ba533e523ef693fc6f84e4e9c8ef3bfc
Joris van Zundert <joris.van.zundert@gmail.com>
2012-01-20 14:03:49 +0100

    Scaling.

--- root/js/componentload.js
+++ root/js/componentload.js
@@ -1,6 +1,14 @@
 function loadTradition( textid ) {
-	$('#stemma_graph').load( "stemma/" + textid );
-	$('#stemma_graph > svg').width('485px');
-	$('#variant_graph').load( "variantgraph/" + textid );
-	$('#variant_graph > svg').height('300px');
+	$('#stemma_graph').load( "stemma/" + textid , function() {
+    	var stemma_svg_element = $('#stemma_graph svg').svg().svg('get').root();
+    	stemma_svg_element.height.baseVal.value = $('#stemma_graph').height();
+	});
+    $('#variant_graph').load( "variantgraph/" + textid , function() {
+    	var variant_svg_element = $('#variant_graph svg').svg().svg('get').root();
+    	var svg_height = variant_svg_element.height.baseVal.value;
+    	var svg_width = variant_svg_element.width.baseVal.value;
+    	var container_height = $('#variant_graph').height();
+    	variant_svg_element.height.baseVal.value = container_height;
+    	variant_svg_element.width.baseVal.value = (svg_width/svg_height * container_height);
+	});
 }
