ede2f5bff95d2bb792aac47933e3ab00fde6305a
Jess Robinson <j.robinson@shadowcat.co.uk>
2012-05-09 10:06:29 +0000

    Initial docs for stemmaweb user model

--- lib/stemmaweb/Model/User.pm
+++ lib/stemmaweb/Model/User.pm
@@ -8,4 +8,43 @@ extends 'Catalyst::Model::KiokuDB';
 
 has '+model_class' => ( default => 'Text::Tradition::UserStore' );
 
+=head1 NAME
+
+stemmaweb::Model::User - User/Auth KiokuDB model for stemmaweb
+
+=head1 SYNOPSIS
+
+    ## CONFIGURATION, in .conf file (this is the default setting)
+    <Model::User>
+      model_class Text::Tradition::UserStore
+    </Model::User>
+
+    <Plugin::Authentication>
+      <default>
+        <credential>
+           class          'Password'
+           password_field 'password'
+           password_type  'self_check'
+        </credential>
+        <store>
+           class          'Model::KiokuDB'
+           model_name     'User'
+        </store>
+      </default>
+    </Plugin::Authentication>
+
+
+=head1 DESCRIPTION
+
+This is the User Model used as a Authentication Store (using
+::Store::Model::KiokuDB) for stemmaweb user authentication.
+
+This separate model allows us to have self-contained user storage,
+which is replaceable. The default uses Text::Tradition::UserStore and
+stores the Users alongside the Traditions.
+
+To replace the source of users for authentication, add the
+configuration shown in the L</SYNOPSIS> to your stemmaweb.conf file,
+and adjust as necessary.
+
 1;
