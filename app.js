f19a7b57a64c82be18c9fb454ac0099cfad133f9
Tara L Andrews <tla@mit.edu>
2021-08-26 17:48:56 +0200

    remove unneeded routes from app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var apiRouter   = require('./routes/api');
 
 var app = express();
