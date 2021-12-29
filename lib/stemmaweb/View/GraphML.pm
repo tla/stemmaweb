874ddb72b74136d9507903144c66ff3c6033003b
Tara L Andrews <tla@mit.edu>
2021-12-29 15:12:46 +0000

    update for GraphML zip format

--- lib/stemmaweb/View/GraphML.pm
+++ lib/stemmaweb/View/GraphML.pm
@@ -6,13 +6,13 @@ use Encode qw/ encode /;
 
 sub process {
     my ($self, $c) = @_;
-    $c->res->content_type('application/graphml+xml');
+    $c->res->content_type('application/zip');
     $c->res->content_encoding('UTF-8');
     if ($c->stash->{download}) {
         $c->res->header('Content-Disposition',
-            sprintf("attachment; filename=\"%s.xml\"", $c->stash->{name}));
+            sprintf("attachment; filename=\"%s.zip\"", $c->stash->{name}));
     }
-    $c->res->output(encode('UTF-8', $c->stash->{result}));
+    $c->res->output($c->stash->{result});
 }
 
 1;
