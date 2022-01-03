33ff5ac92189a7dfad7777eb36ffc2c0fd6b1ae5
Tara L Andrews <tla@mit.edu>
2022-01-03 09:38:12 +0000

    use d3 to make the tradition list

---
+++ public/js/componentload.js
@@ -0,0 +1,14 @@
+function fetchTraditions() {
+  fetch ('/api/traditions')
+  .then(resp => resp.json())
+  .then(data => {
+    let tlist = d3.select('#traditionList')
+      .selectAll('li')
+      .data(data, d => d.id);
+    tlist.exit().remove();
+    tlist = tlist.enter()
+      .append('li')
+      .merge(tlist);
+    tlist.attr("tradId", d => d.id)
+      .text(d => d.name);
+  })};
