bca30a840ede7f6fc722290d1c8c85a58bb0482e
schiwaa <schiwa.aliabadi@univie.ac.at>
2024-02-09 17:41:28 +0100

    add custom command to log in via UI

---
+++ frontend-e2e/cypress/e2e/homepage.cy.js
@@ -0,0 +1,17 @@
+/* Assert everything is visible for an admin */
+
+import users from '../fixtures/users.json';
+const admin = users.filter(({username}) => username === 'admin@example.org')[0];
+
+beforeEach(() => {
+    cy.visit(`${Cypress.env('CY_STEMMAWEB_FRONTEND_URL')}/`);
+    cy.loginViaUi(admin);
+});
+
+describe('all traditions are listed', () => {
+    it('under construction', () => {
+        cy.get('ul#traditions-list > li .tradition-nav-name').each(($ele, index) => {
+            cy.log( index+1 + '. li-text: ' + $ele.text().trim());
+        });
+    });
+});
