abbc17edbbbdb45a6bf9212c75e355f8fc08a341
schiwaa <schiwa.aliabadi@univie.ac.at>
2024-02-12 16:05:50 +0100

    on the homepage, the admin should see all traditions listed

--- frontend-e2e/cypress/e2e/homepage.cy.js
+++ frontend-e2e/cypress/e2e/homepage.cy.js
@@ -1,5 +1,6 @@
-/* Assert everything is visible for an admin */
+/* Assert everything is visible for an admin on the homepage upon login */
 
+import test_traditions from '../fixtures/test_traditions.json';
 import users from '../fixtures/users.json';
 const admin = users.filter(({username}) => username === 'admin@example.org')[0];
 
@@ -13,9 +14,14 @@ afterEach(() => {
   });
 
 describe('all traditions are listed', () => {
-    it('under construction', () => {
-        cy.get('ul#traditions-list > li .tradition-nav-name').each(($ele, index) => {
-            cy.log( index+1 + '. li-text: ' + $ele.text().trim());
+    it('passes', () => {
+        // the number of displayed traditions should be equal to the total number of test_traditions
+        const count = test_traditions.length; // 7
+        cy.get('ul#traditions-list').children().should('have.length', count);
+        test_traditions.forEach((tradition) => {
+            cy.log("title: " + tradition.title);
+            // the test_tradition titles should all be found on the homepage
+            cy.get('ul#traditions-list').contains(tradition.title).should('be.visible');
         });
     });
 });
