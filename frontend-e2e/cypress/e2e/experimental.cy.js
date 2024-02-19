5e2e8200dc66c91608bb379ab8849618e9e0c48b
schiwaa <schiwa.aliabadi@univie.ac.at>
2024-02-19 19:03:57 +0100

    indentation

--- frontend-e2e/cypress/e2e/experimental.cy.js
+++ frontend-e2e/cypress/e2e/experimental.cy.js
@@ -14,28 +14,28 @@ describe('intercept traditions', () => {
 
 describe('intercept login request', () => {
   if (Cypress.env('CY_MODE') === 'headed') { // skip when in headless mode
-  it('passes in headed mode but fails in headless mode: run only in headed mode', { defaultCommandTimeout: 10000 }, () => {
-    cy.log("Cypress.env('CY_MODE'): " + Cypress.env('CY_MODE'));
+    it('passes in headed mode but fails in headless mode: run only in headed mode', { defaultCommandTimeout: 10000 }, () => {
+      cy.log("Cypress.env('CY_MODE'): " + Cypress.env('CY_MODE'));
 
-    cy.visit(`${Cypress.env('CY_STEMMAWEB_FRONTEND_URL')}/`);
-    cy.viewport(1600, 900);
+      cy.visit(`${Cypress.env('CY_STEMMAWEB_FRONTEND_URL')}/`);
+      cy.viewport(1600, 900);
 
-    cy.intercept('POST', `${Cypress.env('CY_STEMMAWEB_FRONTEND_URL')}/requests/login`).as('loginrequest');
+      cy.intercept('POST', `${Cypress.env('CY_STEMMAWEB_FRONTEND_URL')}/requests/login`).as('loginrequest');
 
-    cy.get('header').contains('a', 'Sign in').wait(500).click();
-    cy.get('#loginEmail').wait(500).type('user@example.org', { delay: 50 });
-    cy.get('#loginPassword').wait(500).type('UserPass', { delay: 50 });
-    cy.get('auth-modal').contains('button', 'Sign in').wait(500).click();
+      cy.get('header').contains('a', 'Sign in').wait(500).click();
+      cy.get('#loginEmail').wait(500).type('user@example.org', { delay: 50 });
+      cy.get('#loginPassword').wait(500).type('UserPass', { delay: 50 });
+      cy.get('auth-modal').contains('button', 'Sign in').wait(500).click();
 
-    cy.wait('@loginrequest').then(interception => {
-      // const res_str = JSON.stringify(interception.response);
-      // cy.log('res_str: ' + res_str);
-      cy.expect(interception.response.statusCode).to.eq(200);
+      cy.wait('@loginrequest').then(interception => {
+        // const res_str = JSON.stringify(interception.response);
+        // cy.log('res_str: ' + res_str);
+        cy.expect(interception.response.statusCode).to.eq(200);
+      });
+
+      cy.get('header').contains('a', 'Logged in as user@example.org');
+      cy.get('header').should('not.contain', 'Sign in');
+      cy.get('header').contains('a', 'Sign out'); // for now, don't click without interception
     });
-
-    cy.get('header').contains('a', 'Logged in as user@example.org');
-    cy.get('header').should('not.contain', 'Sign in');
-    cy.get('header').contains('a', 'Sign out'); // for now, don't click without interception
-  });
-}
+  }
 });
--- frontend-e2e/cypress/support/commands.js
+++ frontend-e2e/cypress/support/commands.js
@@ -29,15 +29,15 @@
 Cypress.Commands.add('loginViaUi', (userObj) => {
     if (Cypress.env('CY_MODE') === 'headed') { // skip when in headless mode
         cy.log("Cypress.env('CY_MODE'): " + Cypress.env('CY_MODE'));
-    cy.contains('header a', 'Sign in').click();
-    cy.get('#loginEmail').wait(500).type(userObj.username, { delay: 50 });
-    cy.get('#loginPassword').wait(500).type(userObj.password, { delay: 50 });
-    cy.get('button').contains('Sign in').wait(500).click();
-    cy.get('#authModal').should('not.be.visible');
-    cy.contains('Logged in as ' + userObj.username);
-    cy.contains('header a', 'Sign out');
-    cy.get('header').should('not.contain', 'Sign in');
-    cy.log('Signed in as ' + userObj.username + '!');
+        cy.contains('header a', 'Sign in').click();
+        cy.get('#loginEmail').wait(500).type(userObj.username, { delay: 50 });
+        cy.get('#loginPassword').wait(500).type(userObj.password, { delay: 50 });
+        cy.get('button').contains('Sign in').wait(500).click();
+        cy.get('#authModal').should('not.be.visible');
+        cy.contains('Logged in as ' + userObj.username);
+        cy.contains('header a', 'Sign out');
+        cy.get('header').should('not.contain', 'Sign in');
+        cy.log('Signed in as ' + userObj.username + '!');
     }
 });
 
@@ -46,8 +46,8 @@ Cypress.Commands.add('loginViaUi', (userObj) => {
 Cypress.Commands.add('logoutViaUi', (userObj) => {
     if (Cypress.env('CY_MODE') === 'headed') { // skip when in headless mode
         cy.log("Cypress.env('CY_MODE'): " + Cypress.env('CY_MODE'));
-    cy.contains('header a', 'Sign out').click();
-    cy.contains('header a', 'Sign in');
-    cy.get('header').should('not.contain', 'Sign out');
+        cy.contains('header a', 'Sign out').click();
+        cy.contains('header a', 'Sign in');
+        cy.get('header').should('not.contain', 'Sign out');
     }
 });
