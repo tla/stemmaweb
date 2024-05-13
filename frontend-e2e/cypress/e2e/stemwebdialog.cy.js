6b79ce86950a7f479e0ab246d04e9d41c46504e0
schiwaa <schiwa.aliabadi@univie.ac.at>
2024-05-13 13:13:26 +0200

    assert upon click on another tradition the err and ok messages stay in the message console

--- frontend-e2e/cypress/e2e/stemwebdialog.cy.js
+++ frontend-e2e/cypress/e2e/stemwebdialog.cy.js
@@ -286,7 +286,7 @@ describe('stemma editor tools and svg work properly', () => {
         });
     });
 
-    it.only ('under construction', () => { // needs login
+    it('passes', () => { // needs login
         if (Cypress.env('CY_MODE') === 'headed') { // only log in if headed. dont run this test headless because it needs to be logged in // TODO: also for headless mode
         // TODO: when fitted also for healess mode, merge with previous test (partly duplicate)
         cy.loginViaUi(admin); // TODO: also for headless mode
