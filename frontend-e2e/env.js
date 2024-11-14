c2ccd7fb20e5315b4e3592a0ac9f3759dd1eb9a3
schiwaa <schiwa.aliabadi@univie.ac.at>
2024-11-14 19:49:51 +0100

    include the stemmarest endpoint to env files for cypress

--- frontend-e2e/env.js
+++ frontend-e2e/env.js
@@ -17,10 +17,16 @@ const CY_STEMMAWEB_MIDDLEWARE_URL =
   process.env.CY_STEMMAWEB_MIDDLEWARE_URL ||
   'http://localhost:8888/stemmaweb/requests';
 
+const CY_STEMMAREST_ENDPOINT = 
+  process.env.CY_STEMMAREST_ENDPOINT ||
+  'http://localhost:8080/stemmarest';
+/*   'http://127.0.0.1:8080/stemmarest'; */
+
 const CY_MODE = process.env.CY_STEMMAWEB_FRONTEND_URL ? 'headless' : 'headed';
 
 module.exports = {
   CY_MODE,
   CY_STEMMAWEB_FRONTEND_URL,
-  CY_STEMMAWEB_MIDDLEWARE_URL
+  CY_STEMMAWEB_MIDDLEWARE_URL,
+  CY_STEMMAREST_ENDPOINT
 };
