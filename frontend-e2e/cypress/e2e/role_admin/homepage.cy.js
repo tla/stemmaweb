902742190d983ae0fc4ec8e10401103661a8930a
schiwaa <schiwa.aliabadi@univie.ac.at>
2025-02-14 11:38:11 +0100

    sort test_traditions explicitly to assert they are displayed in alphabetical order

--- frontend-e2e/cypress/e2e/role_admin/homepage.cy.js
+++ frontend-e2e/cypress/e2e/role_admin/homepage.cy.js
@@ -58,7 +58,9 @@ describe('Assert that only one tradition is highlighted in the sidebar menu: \
     it('passes', () => {
         let n = 0 // check the first tradition at start
         test_traditions.forEach((tradition, i) => {
-            // traditions are displayed in alphabetical order (test_traditions sorted above)
+            // assert that traditions are displayed in alphabetical order
+            // sort test_traditions explicitly
+            test_traditions.sort( (tradition_a, tradition_b) => tradition_a.title.localeCompare( tradition_b.title ) );
             cy.log('idx+1) test_tradition title: ' + String(Number(i)+1) + ') ' +tradition.title);
             cy.get('ul#traditions-list > li').eq(i).find('a')
             .invoke('text')
