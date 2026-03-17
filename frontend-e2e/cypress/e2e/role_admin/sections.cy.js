9222d8ee8b926c7b15b75fc0d1985d2e318cab3a
schiwaa <schiwa.aliabadi@univie.ac.at>
2026-03-17 17:20:21 +0100

    lengthen the viewport to see all sections

--- frontend-e2e/cypress/e2e/role_admin/sections.cy.js
+++ frontend-e2e/cypress/e2e/role_admin/sections.cy.js
@@ -39,7 +39,7 @@ if (Cypress.browser.isHeaded) {
 
   beforeEach(() => {
     cy.visit(`${Cypress.env('CY_STEMMAWEB_FRONTEND_URL')}/`);
-    cy.viewport(1600, 900);
+    cy.viewport(1600, 1900);
     test_traditions.sort((tradition_a, tradition_b) =>
       tradition_a.title.localeCompare(tradition_b.title)
     );
