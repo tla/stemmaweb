f9ccf620a323db64629c96c2eb9f83e19a937c26
schiwaa <schiwa.aliabadi@univie.ac.at>
2026-03-18 18:53:14 +0100

    add TODO: reset viewport to landscape format when the 50 added sections are accessible

--- frontend-e2e/cypress.config.js
+++ frontend-e2e/cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   e2e: {
     // specPattern : 'cypress/e2e/experimental.cy.js', // default:: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
 
+    // TODO: reset viewport to landscape format (e.g.1600 x 900) when the 50 added sections are accessible
     viewportWidth: 1600,
     viewportHeight: 1900,
 
