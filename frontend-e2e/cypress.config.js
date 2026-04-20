177a9614ccc7abe83ed54997f78447cddfec5608
schiwaa <schiwa.aliabadi@univie.ac.at>
2026-04-20 21:04:52 +0200

    more sections => higher viewport, for the moment

--- frontend-e2e/cypress.config.js
+++ frontend-e2e/cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
 
     // TODO: reset viewport to landscape format (e.g.1600 x 900) when the 50 added sections are accessible
     viewportWidth: 1600,
-    viewportHeight: 2800,
+    viewportHeight: 3200,
 
     // other e2e options...
 
